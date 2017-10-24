9558cce4a95d37f5b3d5a15ab32bc72da44e24f5
donghyun <kei99134@naver.com>
2017-10-24 17:57:54 +0900

    google map

--- app/src/main/java/com/example/ssgt/MainActivity.java
+++ app/src/main/java/com/example/ssgt/MainActivity.java
@@ -1,39 +1,49 @@
 package com.example.ssgt;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     Button btn_login;
-
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-  //메인메뉴 가는 코드
-        Toast.makeText(getApplicationContext(),"dd",Toast.LENGTH_LONG);
-        Intent intent = new Intent(getApplicationContext(), MenuActivity.class );
+
+        final CustomMapFragment map = (CustomMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        map.getMapAsync(this);
+
+        //메인메뉴 가는 코드
+        Toast.makeText(getApplicationContext(), "dd", Toast.LENGTH_LONG);
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
         startActivity(intent);
 
-        btn_login = (Button)findViewById(R.id.login);
+        btn_login = (Button) findViewById(R.id.login);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-        //hi branch되나볼까나 흠 합쳐지나
+                //hi branch되나볼까나 흠 합쳐지나
 
 
-
-                Intent i = new Intent(getApplicationContext(),InterestActivity.class);
+                Intent i = new Intent(getApplicationContext(), InterestActivity.class);
                 startActivity(i);
 
             }
@@ -41,7 +51,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
+        mMap = googleMap;
 
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setTrafficEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
+    }
 }
